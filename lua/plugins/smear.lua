a6f16b2fa959d6e3ebf4361acba895df49e69ebf
MrGrinch <2003kevinle@gmail.com>
2025-07-15 22:18:03 -0700

    final edit of plugins ... for now

---
+++ lua/plugins/smear.lua
@@ -0,0 +1,16 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    enabled = true,
+    smear_between_buffers = false,
+    stiffness = 0.6,
+    trailing_stiffness = 0.5,
+    time_interval = 7,
+    distance_stop_animating = 0.5,
+    stiffness_insert_mode = 0.7,
+    trailing_stiffness_insert_mode = 0.7,
+    damping = 0.8,
+    damping_insert_mode = 0.8,
+  },
+}
